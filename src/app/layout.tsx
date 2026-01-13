d445efd230b1683a41bae98187590c3dc5c83577
Alvaro Castillo <alvaro.castillo777@gmail.com>
2026-01-12 23:32:28 -0600

    feat: add noscript fallback for Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -96,6 +96,17 @@ export default function RootLayout({
         <Toaster richColors position="top-center" />
         <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID!} />
       </body>
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID!}`}
+          height="0"
+          width="0"
+          style={{
+            display: "none",
+            visibility: "hidden",
+          }}
+        />
+      </noscript>
     </html>
   );
 }
